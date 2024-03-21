dd1bf13bf2026c224e85205e9ec1bdfa2a2fd6b5
rupen-pedalsup <rupen.jarsaniya@pedalsup.com>
2024-03-21 12:10:22 +0530

    Update lint-staged configuration

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express()
-const port = 3000
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`Application is listening at http://localhost:${port}`);
